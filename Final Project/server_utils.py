3ecd0f13912245e0d3aa07ecd2fe6f1db8b1bf74
Julia Fdz <j.fernandezs.2020@alumnos.urjc.es>
2021-06-02 14:07:21 +0200

    new

--- Final Project/server_utils.py
+++ Final Project/server_utils.py
@@ -114,7 +114,7 @@ def karyotype(specie, list):
     if specie == list[0]:
         kary = ['http://www.ensembl.org/Homo_sapiens/Location/Genome',1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 'x', 'y']
     elif specie == list[1]:
-        kary= ['http://www.ensembl.org/Felis_catus/Location/Genome','A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'B4','C1', 'C2', 'D1', 'D2', 'D3', 'D4','E1', 'E2', 'E3', 'F1', 'F2', 'x']
+        kary = ['http://www.ensembl.org/Felis_catus/Location/Genome','A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'B4','C1', 'C2', 'D1', 'D2', 'D3', 'D4','E1', 'E2', 'E3', 'F1', 'F2', 'x']
     elif specie == list[2]:
         kary = ['http://www.ensembl.org/Mus_musculus/Location/Genome', 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 'x', 'y']
     elif specie == list[3]:
@@ -139,11 +139,106 @@ def karyotype(specie, list):
     contents = read_template_html_file('HTML/karyotype.html').render(context=context)
     return contents
 def length( specie, chromosome, list):
-    if specie == list[1]:
+    direccion = ''
+    if specie == list[0]:
         kary = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13,
                 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 'x', 'y']
         while True and i < len(kary):
             i = 0
-            if chromosome == kary[i]:
-                
+            if chromosome == str(kary[i]):
+                direccion = 'http: // www.ensembl.org / Homo_sapiens / Location / Chromosome?r = ' + str(chromosome)
+                False
+            else:
+                i += 1
+    elif specie == list[1]:
+        kary = ['A1', 'A2', 'A3', 'B1', 'B2', 'B3', 'B4', 'C1',
+                'C2', 'D1', 'D2', 'D3', 'D4', 'E1', 'E2', 'E3', 'F1', 'F2', 'x']
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion = 'http://www.ensembl.org/Felis_catus/Location/Chromosome?r=' + str(chromosome)
+                False
+            else:
+                i += 1
+    elif specie == list[2]:
+        kary = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 'x', 'y']
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion = 'http://www.ensembl.org/Mus_musculus/Location/Chromosome?r=' + str(chromosome)
+                False
+            else:
+                i += 1
+    elif specie == list[3]:
+        kary = ['Not provided']
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion == 'error'
+                False
+            else:
+                i += 1
+    elif specie == list[4]:
+        kary = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 'x', 'y']
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion = 'http://www.ensembl.org/Balaenoptera_musculus/Location/Chromosome?r=' + str(chromosome)
+                False
+            else:
+                i += 1
+    elif specie == list[5]:
+        kary = [ 1, '2A', '2B', 3, 4, 5, 6, 7, 8, 9, 10, 11,
+                12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 'x']
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion = 'http://www.ensembl.org/Gorilla_gorilla/Location/Chromosome?r=' + str(chromosome)
+                False
+            else:
+                i += 1
+    elif specie == list[6]:
+        kary = [1, 2, 3, 4, '4A', 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 24, '25LG1', '25LG2', 26, 27, 28, 'LGE22', 'Z']
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion = 'http://www.ensembl.org/Parus_major/Location/Chromosome?r=' + str(chromosome)
+                False
+            else:
+                i += 1
+    elif specie == list[7]:
+        kary = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29]
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion = 'http://www.ensembl.org/Ictalurus_punctatus/Location/Chromosome?r=' + str(chromosome)
+                False
+            else:
+                i += 1
+    elif specie == list[8]:
+        kary = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 'x', 'y']
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion = 'http://www.ensembl.org/Sciurus_vulgaris/Location/Chromosome?r=' + str(chromosome)
+                False
+            else:
+                i += 1
 
+    elif specie == list[9]:
+        kary = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
+                16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
+        while True and i < len(kary):
+            i = 0
+            if chromosome == str(kary[i]):
+                direccion = 'http://www.ensembl.org/Danio_rerio/Location/Chromosome?r=' + str(chromosome)
+                False
+            else:
+                i += 1
+    context = {
+        'Specie': specie,
+        "Chromosome": chromosome,
+        'Direction': direccion
+    }
+    contents = read_template_html_file('HTML/length.html').render(context=context)
+    return contents
